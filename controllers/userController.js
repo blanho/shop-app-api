a8ed0c8ba516df313328786c0e67a9f56ec1b3e0
blanho <h.baolan20025@gmail.com>
2022-09-13 14:31:57 +0700

    User - Update Password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const { StatusCodes } = require("http-status-codes");
-const { NotFound } = require("../errors");
+const { NotFound, BadRequest } = require("../errors");
 const User = require("../models/User");
 const checkUserAuthorization = require("../utils/checkAuthorization");
 
@@ -19,6 +19,9 @@ const getSingleUser = async (req, res) => {
   if (!user) {
     throw new NotFound(`Cannot find this user with id: ${id}`);
   }
+  // Find incoming user matches with user that is being found
+  // Admin has the permission to get single user
+
   checkUserAuthorization(req.user, user);
 
   res.status(StatusCodes.OK).json({ user });
@@ -33,7 +36,23 @@ const updateUserInfo = async (req, res) => {
 };
 
 const updatePassword = async (req, res) => {
-  res.send("Update Password");
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new BadRequest("Please provide all values");
+  }
+  const user = await User.findOne({ _id: req.user.userId });
+
+  // Admin has the permission to change user password
+  checkUserAuthorization(req.user, user);
+
+  const isCorrectPassword = await user.comparePassword(oldPassword);
+  if (!isCorrectPassword) {
+    throw new BadRequest("Please provide a valid password");
+  }
+  user.password = newPassword;
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: "Updated Password Successfully" });
 };
 
 const deleteUser = async (req, res) => {
@@ -43,7 +62,7 @@ const deleteUser = async (req, res) => {
   if (!user) {
     throw new NotFound(`Cannot find this user with id: ${id}`);
   }
-  user.remove();
+  await user.remove();
 
   res.status(StatusCodes.OK).json({ msg: "Deleted Successfully" });
 };
