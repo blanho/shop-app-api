d87848592dd09cbc3e77b2b95b6c4ca8e70347ef
blanho <h.baolan20025@gmail.com>
2022-09-13 14:11:25 +0700

    User - Show Current User

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,7 +25,7 @@ const getSingleUser = async (req, res) => {
 };
 
 const showCurrentUser = async (req, res) => {
-  res.send("Show current user");
+  res.status(StatusCodes.OK).json({ user: req.user });
 };
 
 const updateUserInfo = async (req, res) => {
