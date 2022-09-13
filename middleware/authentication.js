240405a55a5f507ce5768ff47158a88c783bd022
blanho <h.baolan20025@gmail.com>
2022-09-13 10:05:41 +0700

    Middleware - Authentication

---
+++ middleware/authentication.js
@@ -0,0 +1,36 @@
+const { Unauthenticated } = require("../errors");
+const Token = require("../models/Token");
+const { verifyJWT, attachJWTtoCookies } = require("../utils/jwt");
+
+const authenticatedUser = async (req, res, next) => {
+  const { accessToken, refreshToken } = req.signedCookies;
+
+  try {
+    if (accessToken) {
+      const payload = verifyJWT(accessToken);
+      req.user = payload;
+      return next();
+    }
+    const payload = verifyJWT(refreshToken);
+
+    const existingToken = await Token.findOne({
+      refreshToken: payload.refreshToken,
+      user: payload.user.userId,
+    });
+
+    if (!existingToken || !existingToken?.isValid) {
+      throw new Unauthenticated("Unauthenticated User");
+    }
+    attachJWTtoCookies({
+      res,
+      payload: payload.user,
+      refreshToken: existingToken.refreshToken,
+    });
+    req.user = payload.user;
+    next();
+  } catch (error) {
+    throw new Unauthenticated("Unauthenticated User");
+  }
+};
+
+module.exports = authenticatedUser;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,11 +9,12 @@ const {
   resetPassword,
   verifyEmail,
 } = require("../controllers/authController");
+const authenticatedUser = require("../middleware/authentication");
 
 router.post("/register", register);
 router.post("/verify-email", verifyEmail);
 router.post("/login", login);
-router.delete("/logout", logout);
+router.delete("/logout", authenticatedUser, logout);
 
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
